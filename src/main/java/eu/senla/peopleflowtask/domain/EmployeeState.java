a896618c312885f13fc0d95c5a09eddc8166e351
ulyanaprovalskaya <ulyana.provalskaya@gmail.com>
2021-09-09 14:29:38 +0300

    deleted comments

--- src/main/java/eu/senla/peopleflowtask/domain/EmployeeState.java
+++ src/main/java/eu/senla/peopleflowtask/domain/EmployeeState.java
@@ -1,17 +1,8 @@
 package eu.senla.peopleflowtask.domain;
 
-import lombok.Getter;
-
 public enum EmployeeState {
     ADDED,
     IN_CHECK,
     APPROVED,
-    ACTIVE;
-
-    //public abstract EmployeeState nextState();
-    //public abstract EmployeeState previousState();
-
-    /*public boolean canChangeState(EmployeeState target) {
-        return this.nextState().equals(target) || this.previousState().equals(target);
-    }*/
+    ACTIVE
 }
