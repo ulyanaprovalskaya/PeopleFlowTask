cb4a10aa32ab0c49b9aaf3161091d6be09839fd8
ulyanaprovalskaya <ulyana.provalskaya@gmail.com>
2021-09-09 16:07:46 +0300

    removed unused imports

--- src/test/java/eu/senla/peopleflowtask/controller/EmployeeControllerTest.java
+++ src/test/java/eu/senla/peopleflowtask/controller/EmployeeControllerTest.java
@@ -14,13 +14,11 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
